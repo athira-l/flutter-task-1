d3620244e68ba549b454faf5437b0ecc5e32f9d4
Athira <athira.l@labournet.in>
2022-11-16 15:09:47 +0530

    v1

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,13 +1,21 @@
 import 'package:get/get.dart';
 
 class Provider extends GetConnect{
-
+final String _baseUrl="https://reqres.in/api";
   Future<List<dynamic>> getUser() async{
-    final response = await get('https://reqres.in/api/users?per_page=12');
+    final response = await get('$_baseUrl/users?per_page=12');
     if(response.status.hasError){
       return Future.error(response.statusText!);
     } else {
       return response.body['data'];
+    }
+  }
+  Future<dynamic> getUserDetail(int id) async {
+    final response = await get('$_baseUrl/users/$id');
+    if(response.status.hasError){
+      return Future.error(response.statusText!);
+    } else {
+      return response.body;
     }
   }
 }
